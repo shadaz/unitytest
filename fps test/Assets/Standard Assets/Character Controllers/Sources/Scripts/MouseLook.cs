f47ab1a87ec27d37886b85ef969d5bc6263d07f6
Shadaz <shadaz@pobox.com>
2015-01-19 15:59:29 -0800

    misc updates

--- fps test/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ fps test/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -6,58 +6,82 @@ using System.Collections;
 
 /// To make an FPS style character:
 /// - Create a capsule.
+/// - Add a rigid body to the capsule
 /// - Add the MouseLook script to the capsule.
 ///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
-/// - Add FPSInputController script to the capsule
-///   -> A CharacterMotor and a CharacterController component will be automatically added.
+/// - Add FPSWalker script to the capsule
 
 /// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
 /// - Add a MouseLook script to the camera.
 ///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
 [AddComponentMenu("Camera-Control/Mouse Look")]
-public class MouseLook : MonoBehaviour {
+public class MouseLook : MonoBehaviour
+{
 
-	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 15F;
-	public float sensitivityY = 15F;
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
 
-	public float minimumX = -360F;
-	public float maximumX = 360F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
 
-	public float minimumY = -60F;
-	public float maximumY = 60F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
 
-	float rotationY = 0F;
+    float rotationX = 0F;
+    float rotationY = 0F;
 
-	void Update ()
-	{
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-		}
-		else
-		{
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-		}
-	}
-	
-	void Start ()
-	{
-		// Make the rigid body not change rotation
-		if (GetComponent<Rigidbody>())
-			GetComponent<Rigidbody>().freezeRotation = true;
-	}
+    Quaternion originalRotation;
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            // Read the mouse input axis
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+            transform.localRotation = originalRotation * yQuaternion;
+        }
+    }
+
+    void Start()
+    {
+        // Make the rigid body not change rotation
+        if (rigidbody)
+            rigidbody.freezeRotation = true;
+        originalRotation = transform.localRotation;
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
 }
--- fps test/Assets/animations.js
+++ fps test/Assets/animations.js
@@ -1,42 +1,75 @@
 ﻿#pragma strict
 internal var animator : Animator;
+internal var cc : CharacterController;
 var h : float;
 var v : float;
 var sprint : float;
+var slide : boolean;
+var sprintjumping : boolean;
+var Climb : boolean;
 
 
 function Start () 
 {
 animator = GetComponent(Animator);
-
+cc = GetComponent(CharacterController);
 
 }
 
 function Update () 
 {
-
 v = Input.GetAxis("Vertical");
 h = Input.GetAxis("Horizontal");
-Sprinting();
+if (Input.GetKey(KeyCode.LeftShift))
+ {
+sprint = 0.2;
+ }
+else
+ {	
+sprint = 0.0;
+ }
 
 
-}
+// slide
+if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKeyDown(KeyCode.LeftControl))
+	{
+slide = true;
 
-function FixedUpdate ()
-{
+	}
+else
+	{
+slide = false;
+
+	}
+
+// sprint jump
+if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKeyDown(KeyCode.Space))
+	{
+sprintjumping = true;
+	}
+else
+	{
+sprintjumping = false;
+	}
+	
+// climb / jump
+if (Input.GetKeyDown(KeyCode.Space))
+	{
+Climb = true;
+	}
+else
+	{
+Climb = false;
+	}	
+	
 animator.SetFloat("Walk", v);
 animator.SetFloat("Turn", h);
 animator.SetFloat("Sprint", sprint);
+animator.SetBool("slide", slide);
+animator.SetBool("sprintjumping", sprintjumping);
+animator.SetBool("Climb", Climb);
 
 }
 
-function Sprinting ()
-{
-if (Input.GetButton("Fire1")){
-sprint = 0.2;
-	}
-else{
-	sprint = 0.0;
-	}
 
-}
+
