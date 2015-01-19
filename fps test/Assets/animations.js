eebcb33499ec693bb81dd352e3411fa90b1486c0
Shadaz <shadaz@pobox.com>
2015-01-18 16:04:21 -0800

    Initial commit

---
+++ fps test/Assets/animations.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+internal var animator : Animator;
+var h : float;
+var v : float;
+var sprint : float;
+
+
+function Start () 
+{
+animator = GetComponent(Animator);
+
+
+}
+
+function Update () 
+{
+
+v = Input.GetAxis("Vertical");
+h = Input.GetAxis("Horizontal");
+Sprinting();
+
+
+}
+
+function FixedUpdate ()
+{
+animator.SetFloat("Walk", v);
+animator.SetFloat("Turn", h);
+animator.SetFloat("Sprint", sprint);
+
+}
+
+function Sprinting ()
+{
+if (Input.GetButton("Fire1")){
+sprint = 0.2;
+	}
+else{
+	sprint = 0.0;
+	}
+
+}
